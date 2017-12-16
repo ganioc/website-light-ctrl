10b199db6ed8a5f69ac6b229f32d812b0155db14
yang <yangjun@nanchao.org>
2017-12-16 19:39:38 +0800

    Add modal dlg

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,19 @@
 console.log("hello");
 
+// 
+
+$("#btnOn").click(() => {
+    console.log("Click-on");
+    $("#dlgModal").modal("show");
+});
+
+
+$("#btnOff").click(() => {
+    console.log("Click-off");
+    $("#dlgModal").modal("hide");
+});
+
+$("#btnQuery").click(() => {
+    console.log("Click-query");
+    $("#dlgModal").modal("show");
+});
